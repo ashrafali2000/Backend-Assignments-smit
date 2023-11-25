fb925cf0f141c939b89b83203232aa96e887d060
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-25 12:30:38 +0500

    add middleware in app file

--- express-ejs-assignment-1/app.js
+++ express-ejs-assignment-1/app.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const newProduct = require("./routes/product");
+const path = require("path");
+const chalk = require("chalk")
+const methodOverride = require('method-override')
+const mongoose = require("./config/connection")
 
 
-const PORT = 3000;
 const app = express();
-app.listen(() => {
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+// Server Port
+const PORT = 3000;
+
+// Set Vies Engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Static file add
+app.use(express.static(path.join(process.cwd(), "public")))
+
+// db CONNECTION
+const db = mongoose.connection;
+db.on("error",console.error.bind( console, "Connection Error"));
+db.once("open", function (){
+  console.log(chalk.bgGreen("db Connected"));
+})
+
+app.use("/products", newProduct);
+
+
+
+
+
+// Server Listen
+app.listen(3000,() => {
 console.log(`Server is running on PORT : ${PORT}`)
 })
